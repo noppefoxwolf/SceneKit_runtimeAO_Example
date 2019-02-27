5c8842291f4a827cd6c69c6e395109c5fa85ed2f
noppefoxwolf <noppelabs@gmail.com>
2019-02-28 03:31:28 +0900

    実装

--- AO/GameViewController.swift
+++ AO/GameViewController.swift
@@ -9,113 +9,93 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import SceneKit.ModelIO
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
-        
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
-        
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // create a new scene
+    let scene = SCNScene(named: "art.scnassets/ship.scn")!
+    
+    // create and add an ambient light to the scene
+    let ambientLightNode = SCNNode()
+    ambientLightNode.light = SCNLight()
+    ambientLightNode.light!.type = .ambient
+    ambientLightNode.light!.color = UIColor.white
+    scene.rootNode.addChildNode(ambientLightNode)
+    
+    let plane = SCNPlane()
+    plane.width = 75
+    plane.height = 75
+    plane.name = "com.noppelab.plane"
+    let planeNode = SCNNode()
+    let d = -90 * (Float.pi / 180)
+    planeNode.eulerAngles = .init(d, 0, 0)
+    planeNode.position = .init(0, 0, 0)
+    planeNode.geometry = plane
+    planeNode.name = "com.noppelab.plane"
+    scene.rootNode.addChildNode(planeNode)
+    
+    // retrieve the SCNView
+    let scnView = self.view as! SCNView
+    
+    // set the scene to the view
+    scnView.scene = scene
+    
+    // allows the user to manipulate the camera
+    scnView.allowsCameraControl = true
+    
+    // show statistics such as fps and timing information
+    scnView.showsStatistics = true
+    
+    // configure the view
+    scnView.backgroundColor = UIColor.black
+    
+    bake(nodeName: "com.noppelab.plane")
+  }
+  
+  func flattenObjectInMeshes(parentTransform: inout simd_float4x4, object: MDLObject, meshArray: inout Array<MDLMesh>) {
+    if let transform = object.transform {
+      parentTransform = simd_mul(parentTransform, transform.matrix)
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
-            
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
+    if let mesh = object as? MDLMesh {
+      mesh.transform = MDLTransform(matrix: parentTransform)
+      meshArray.append(mesh)
+    } else {
+      for o in object.children.objects {
+        flattenObjectInMeshes(parentTransform: &parentTransform, object: o, meshArray: &meshArray)
+      }
     }
+  }
+  
+  func flattenMeshes(asset: MDLAsset) -> [MDLMesh] {
+    var parentTransform = matrix_identity_float4x4
+    var meshArray: [MDLMesh] = .init()
+    for i in 0..<asset.count {
+      flattenObjectInMeshes(parentTransform: &parentTransform, object: asset.object(at: i), meshArray: &meshArray)
+    }
+    return meshArray
+  }
+  
+  func bake(nodeName: String) {
+    guard let scnView = self.view as? SCNView, let scene = scnView.scene else {return}
+    let asset = MDLAsset(scnScene: scene)
+    let planeMesh = asset.childObjects(of: MDLMesh.self).filter({ $0.name == nodeName }).first as! MDLMesh
     
-    override var shouldAutorotate: Bool {
-        return true
-    }
+    let rootMeshes = flattenMeshes(asset: asset)
     
-    override var prefersStatusBarHidden: Bool {
-        return true
+    // generateAmbientOcclusionTexture will block so do it on a background thread
+    DispatchQueue.global().async {
+      
+      planeMesh.generateAmbientOcclusionTexture(withQuality: 0.5, attenuationFactor: 0.2, objectsToConsider: rootMeshes, vertexAttributeNamed: MDLVertexAttributeTextureCoordinate, materialPropertyNamed: "ambientOcclusion")
+      
+      DispatchQueue.main.async {
+        let planeNode = scene.rootNode.childNode(withName: nodeName, recursively: true)!
+        planeNode.geometry = SCNGeometry(mdlMesh: planeMesh)
+      }
     }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
+  }
 }
